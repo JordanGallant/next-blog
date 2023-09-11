434d7e9ae866e231c864ff223fb8140a520d6e0e
JordanGallant <39-12602@sae.edu.za>
2023-09-11 09:17:42 +0200

    adding-content

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,11 @@ import "./globals.css";
 export default function Home() {
   return (
     <div>
-      <Link href ="/">Hello</Link>
+      <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum numquam ipsum reiciendis provident quod excepturi incidunt voluptas fugit, et ducimus nobis expedita suscipit alias explicabo nihil sequi corrupti, dolorem odit.</h1>
+      <Link href ="/">Bro ipsum dolor sit amet method whip ollie, Whistler ACL yard sale brain bucket hammer dust on crust daffy. Stoked laps groomer, first tracks slash bonk spread eagle. Corn tele schwag dust on crust stoked air gaper gorby gondy trucks stoked switch park face shots 360. 
+      Dust on crust shuttle moguls DH mute, sharkbite euro first tracks McTwist fully.Ski bum 360 pow pow, pillow popping punter afterbang stoked white room Snowboard free ride. Rail greasy jib acro groomer, steed huck steeps air. Berm manny sketching corn, white room huck bear trap bunny slope. 
+      Poaching hurl carcass carve dust on crust deck back country wheelie drop brain bucket DH grip tape newschooler yard sale taco pow. 180 rail stoked washboard first tracks caballerial, 180 berm poaching stoked liftie tele huck stoked saddle.Travel stunt dope, endo shred face shots slash stoked huckfest single track. 
+      Gnar skid switch wheelie drop, pow pow chain ring dust on crust schraeder washboard. Scream chain ring sucker hole, whip gondy huck ACL shuttle heli schwag pow pow euro. Heli giblets butter carbon poaching. Grind titanium cruiser misty newschooler hardtail chillax dust on crust face plant. Face shots pipe crank, stoked flow poaching Whistler bonk.</Link>
     </div>
   );
 
